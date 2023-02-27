cc1ece072436a54f0333aee686d34140656e6496
abhira1981 <126512106+abhira1981@users.noreply.github.com>
2023-02-27 15:59:21 -0500

    Revert "Made changes"
    This reverts commit bcccd3524775d3e38e29669833dc0f9b6fd90d5d.

--- employee_information/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from employee_information.models import Department, Position, Employees
-
-# Register your models here.
-admin.site.register(Department)
-admin.site.register(Position)
-admin.site.register(Employees)
